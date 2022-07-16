6e62973e08c5e9a5f782a9020e18987bd1641c3f
Christopher Evans <cevans@Macintosh-0016cb94cd0b-3.local>
2022-07-16 09:43:52 -0400

    Finished initial implementation of dark mode

--- docker/PS1.sh
+++ docker/PS1.sh
@@ -1,5 +1,5 @@
 if [ "${DARK_MODE:-1}" -eq "1" ]; then
 	export PS1="\033[38;5;075;48;5;000m\u@docker\[\033[00m\]:\033[38;5;082;48;5;000m\w\[\033[00m\]\$ "
 else
-	echo "Light mode."
+	export PS1="\033[38;5;021m\u@docker\[\033[00m\]:\033[38;5;022m\w\[\033[00m\]\$ "
 fi
