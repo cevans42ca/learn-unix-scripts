379e946863da3a49f6f60d47a73da16e9dad8da4
Christopher Evans <cevans@Macintosh-0016cb94cd0b-3.local>
2022-07-16 14:55:40 -0400

    Cleaned up the fixPermissions script to work with a shared dir

--- docker/fixPermissions.sh
+++ docker/fixPermissions.sh
@@ -6,15 +6,26 @@ full_vol_dir="$vol_dir/lus/_data/$2"
 if [ -d "$vol_dir" ]
 then
 	if [[ "$3" == "shared" ]]; then
-	        sudo mkdir $(full_vol_dir)
-		sudo chgrp $1 $(full_vol_dir)
-		sudo chmod g+w $(full_vol_dir)
-		sudo chmod +t $(full_vol_dir)
+	        sudo mkdir -p "${full_vol_dir}/shared"
+		sudo chgrp $1 "${full_vol_dir}"
+		sudo chmod g+w "${full_vol_dir}"
+		sudo chgrp $1 "${full_vol_dir}/shared"
+		sudo chmod g+w "${full_vol_dir}/shared"
+		sudo chmod +t "${full_vol_dir}/shared"
 	else
 		sudo chmod g+w "$full_vol_dir"
 	fi
 else
-	docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i mkdir -p "$full_vol_dir"
-	docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chgrp $1 "$full_vol_dir"
-	docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chmod g+w "$full_vol_dir"
+	if [[ "$3" == "shared" ]]; then
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i mkdir -p "$full_vol_dir/shared"
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chgrp $1 "$full_vol_dir"
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chmod g+w "$full_vol_dir"
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chgrp $1 "$full_vol_dir/shared"
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chmod g+w "$full_vol_dir/shared"
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chmod +t "$full_vol_dir/shared"
+	else
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i mkdir -p "$full_vol_dir"
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chgrp $1 "$full_vol_dir"
+		docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i chmod g+w "$full_vol_dir"
+	fi
 fi
