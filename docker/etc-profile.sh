264393a5baaffc43785f194100cda1ee9bfdc3af
Chris Evans <github@quine.ca>
2022-07-19 07:34:49 -0400

    Updated light mode prompt to blue

--- docker/etc-profile.sh
+++ docker/etc-profile.sh
@@ -1,7 +1,7 @@
 if [ "${DARK_MODE:-1}" -eq "1" ]; then
 	export PS1="\033[38;5;075;48;5;000m\u@docker\[\033[00m\]:\033[38;5;082;48;5;000m\w\[\033[00m\]\$ "
 else
-	export PS1="\033[38;5;004m\u@docker\[\033[00m\]:\033[38;5;022m\w\[\033[00m\]\$ "
+	export PS1="\033[38;5;020m\u@docker\[\033[00m\]:\033[38;5;022m\w\[\033[00m\]\$ "
 fi
 
 export INSIDE_DOCKER=1
