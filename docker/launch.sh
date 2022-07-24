962682993ac8335584dcac94ce03363f5f9c88d5
Chris Evans <github@quine.ca>
2022-07-24 10:25:52 -0400

    Greatly simplified permissions handling and added a warning on start if progress can't be saved

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo
-echo
-echo 'Type the word "learn" and press Enter to begin.'
-exec bash
+if { [ -w "/mnt/vol/testuser" ] || [ -w "/mnt/vol/shared" ]; }; then
+	echo
+	echo
+	echo 'Type the word "learn" and press Enter to begin.'
+	exec bash
+else
+	echo "Unable to save progress.  Please run the following commands."
+	echo
+	echo "docker volume create lus"
+	echo "docker run --mount src=lus,target=/mnt/vol -u 0 learn-unix /bin/bash -c 'mkdir -p /mnt/vol/testuser;chgrp 1000 /mnt/vol/testuser;chmod g+w /mnt/vol/testuser'"
+fi
