debd87d5b5c4548ac8ef15d2478c4a16103363b5
Chris Evans <github@quine.ca>
2022-07-25 20:24:06 -0400

    The user will now receive help if the volume hasn't been set up correctly

--- docker/launch.sh
+++ docker/launch.sh
@@ -6,8 +6,24 @@ if { [ -w "/mnt/vol/testuser" ] || [ -w "/mnt/vol/shared" ]; }; then
 	echo 'Type the word "learn" and press Enter to begin.'
 	exec bash
 else
-	echo "Unable to save progress.  Please run the following commands."
+	echo "The volume hasn't been set up, which means you won't be able to save"
+	echo "your progress.  What do you want to do?"
 	echo
-	echo "docker volume create lus"
-	echo "docker run --mount src=lus,target=/mnt/vol -u 0 learn-unix /bin/bash -c 'mkdir -p /mnt/vol/testuser;chgrp 1000 /mnt/vol/testuser;chmod g+w /mnt/vol/testuser'"
+	echo "(1) Explore a little anyway"
+	echo "(2) Find out how to set up the volume so you can save your progress"
+	echo "(3) Quit"
+	echo
+	read -p "Please type 1, 2, or 3 and press Enter:  " choice
+
+        if [[ "${choice}" =~ ^[\ ]*1[\ ]*$ ]]; then
+		echo
+		echo 'Type the word "learn" and press Enter to begin.'
+		exec bash
+	elif [[ "${choice}" =~ ^[\ ]*2[\ ]*$ ]]; then
+		echo
+		echo "Run the following commands."
+		echo
+		echo "docker volume create lus"
+		echo "docker run --mount src=lus,target=/mnt/vol -u 0 alpine /bin/ash -c 'mkdir -p /mnt/vol/testuser;chgrp 1000 /mnt/vol/testuser;chmod g+w /mnt/vol/testuser'"
+        fi
 fi
