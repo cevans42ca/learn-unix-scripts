028807d61b646916d182f7cbca820c4928ac0ffc
Chris Evans <github@quine.ca>
2022-07-30 12:30:15 -0400

    Fixed PS1 to reduce terminal garbage; Added an alias to add colour to ls

--- docker/etc-profile.sh
+++ docker/etc-profile.sh
@@ -1,8 +1,10 @@
 if [ "${DARK_MODE:-1}" -eq "1" ]; then
-	export PS1="\033[38;5;075;48;5;000m\u@docker\[\033[00m\]:\033[38;5;082;48;5;000m\w\[\033[00m\]\$ "
+	export PS1="\[\033[38;5;075;48;5;000m\]\u@docker\[\033[00m\]:\[\033[38;5;082;48;5;000m\]\w\[\033[00m\]\$ "
 else
-	export PS1="\033[38;5;020m\u@docker\[\033[00m\]:\033[38;5;022m\w\[\033[00m\]\$ "
+	export PS1="\[\033[38;5;020m\]\u@docker\[\033[00m\]:\[\033[38;5;022m\]\w\[\033[00m\]\$ "
 fi
+
+alias ls='ls --color'
 
 export INSIDE_DOCKER=1
 export LUS_DOCKER_BASE=/mnt/vol/testuser
