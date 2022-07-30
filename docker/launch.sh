afcd5a9b215cb420fdac16775c5e52a5f4815992
Chris Evans <github@quine.ca>
2022-07-30 08:43:00 -0400

    Updated volume setup help; Updated lesson text

--- docker/launch.sh
+++ docker/launch.sh
@@ -21,9 +21,14 @@ else
 		exec bash
 	elif [[ "${choice}" =~ ^[\ ]*2[\ ]*$ ]]; then
 		echo
-		echo "Run the following commands."
-		echo
+		echo "# Setup commands:  run them once"
 		echo "docker volume create lus"
 		echo "docker run --mount src=lus,target=/mnt/vol -u 0 alpine /bin/ash -c 'mkdir -p /mnt/vol/testuser;chgrp 1000 /mnt/vol/testuser;chmod g+w /mnt/vol/testuser'"
+		echo
+		echo "# Work with traditional white text on black terminal settings"
+		echo "docker run -it --rm --mount src=lus,target=/mnt/vol cevans42ca/learn-unix-scripts"
+		echo
+		echo "# Work with black text on white"
+		echo "docker run -it --rm --mount src=lus,target=/mnt/vol --env DARK_MODE=0 docker run -it --rm cevans42ca/learn-unix-scripts"
         fi
 fi
