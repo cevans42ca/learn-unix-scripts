91ed442ee14aa25a53ef26dce3c7fe07dc14f5bb
Chris Evans <github@quine.ca>
2022-09-08 09:53:05 -0400

    Updated exploratory perl highlight code to work with /usr/bin/env

--- main/lus-highlight.pl
+++ main/lus-highlight.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/env perl
 
 use Term::ANSIColor;
-$script_path=$ARGV[0];
 
-if ($ENV{"PATH"} =~ /(^|.*:)(\Q${script_path}\E)(:.*|$)/)
+$path=$ARGV[0]; $script_path=$ARGV[1];
+
+if ("$path" =~ /(^|.*:)(\Q${script_path}\E)(:.*|$)/)
 { print $1 . color('bold red') . $2 . color('reset') . $3 . "\n"; }
 else
-{ print "Unable to find ${script_path} in " . $ENV["PATH"] . ".\n"; }
+{ print "Unable to find ${script_path} in $path.\n"; }
