ad3ab8d0e6f97b1313bf27f2e7474e33c387c733
Chris Evans <github@quine.ca>
2022-09-10 10:24:16 -0400

    Slight changes to handle terminals with non-ANSI color better

--- main/docker/etc-profile.sh
+++ main/docker/etc-profile.sh
@@ -1,7 +1,13 @@
-if [ "${DARK_MODE:-1}" -eq "1" ]; then
-	export PS1="\[\033[38;5;075;48;5;000m\]\u@docker\[\033[00m\]:\[\033[38;5;082;48;5;000m\]\w\[\033[00m\]\$ "
-else
-	export PS1="\[\033[38;5;020m\]\u@docker\[\033[00m\]:\[\033[38;5;022m\]\w\[\033[00m\]\$ "
+if which tput >/dev/null 2>&1; then
+	if [ "$(tput colors)" -le "8" ]; then
+		if [ "${DARK_MODE:-1}" -eq "1" ]; then
+			export PS1="\[\033[38;5;075;48;5;000m\]\u@docker\[\033[00m\]:\[\033[38;5;082;48;5;000m\]\w\[\033[00m\]\$ "
+		else
+			export PS1="\[\033[38;5;020m\]\u@docker\[\033[00m\]:\[\033[38;5;022m\]\w\[\033[00m\]\$ "
+		fi
+	else
+		export PS1="[\u@docker:\w] \$ "
+	fi
 fi
 
 alias ls='ls --color'
